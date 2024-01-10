4e8e2c6f96772004e2ac4606f7709744188be145
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2024-01-10 15:04:26 +0000

    notes added to the main page

--- src/front/js/component/notes.js
+++ src/front/js/component/notes.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
-
+import { useNavigate } from "react-router-dom";
+import "../../styles/components/notes.css"
 const Notes = () =>{
     const [notes, setNotes] = useState([])
+    const navigate = useNavigate()
 
     const getAllNotes = () =>{
         fetch(process.env.BACKEND_URL + 'api/notes', {
@@ -24,22 +26,23 @@ const Notes = () =>{
     const showAllNotes = () =>{
         return notes.map((notes,index)=>{
             return(
-                <div key={index}>
-                    <li>{notes.title}</li>
+                <div key={index} >
+                    <li><h4>{notes.title}</h4></li>
                     <span>{notes.description}</span>
+                    <button className="btn btn-dark dark-text w-100 mt-4 mb-3">Ir</button>
                 </div>
             )
         })
     }
 
     return(
-        <section>
-            <p>Tus notas...</p>
-
-            <ul>
+        <section className="flex flex-column text-center">
+            <h2 className="mb-4">Tus notas...📝</h2>
+            <ul id="notesList">
                 {showAllNotes()}
             </ul>
-            <button className="btn btn-success" onClick={()=>navigate("/notas")}>Notas</button>
+            <span className="text-muted small d-block">{notes.length} Notas añadidas</span>
+            <button className="btn btn-success my-4" onClick={()=>navigate("/notas")}>Notas</button>
         </section>
     )
 }
