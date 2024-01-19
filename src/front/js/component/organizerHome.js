9f7533c1e7750fc8cbb4211ddfd7f8e1e598fa85
Edi Javier <126507385+edijavier99@users.noreply.github.com>
2024-01-19 21:15:26 +0000

    small bug checked

--- src/front/js/component/organizerHome.js
+++ src/front/js/component/organizerHome.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const OrganizerHome = () =>{
+    const navigate = useNavigate()
+
     return(
     <section className="flex flex-column text-center">
         <h2 className="">Organizador</h2>
