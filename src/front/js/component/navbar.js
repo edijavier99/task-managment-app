97b816b8566f5c3e9b0a5695d4d73e74bcd49c84
Edison Cañizares <126507385+edijavier99@users.noreply.github.com>
2024-07-10 12:44:49 +0000

    logo image added

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
+import logo from "./../../img/logo.png"
 
 export const Navbar = () => {
 	const navigate = useNavigate()
@@ -10,7 +11,7 @@ export const Navbar = () => {
 	return (
 	<nav className="navbar navbar-expand-lg navbar-light">
 		<div className="container-fluid">
-			<a className="navbar-brand" href="#">Navbar</a>
+			<a className="navbar-brand" href="#"><img src={logo} alt="logo" style={{height: "50"}} /></a>
 			{currentUser ? <i className="fa-solid fa-right-to-bracket" onClick={()=> actions.logOut()}></i> :
 			 <i className="fa-solid fa-user" onClick={() => navigate('/login')}></i>}		
 		</div>
